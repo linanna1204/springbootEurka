1142c672e7a34baea4754e6b8a3f4c737bb8d15a
linanan <18852678879@163.com>
2021-06-02 10:33:23 +0800

    master commit2

--- src/main/java/sun/Test.java
+++ src/main/java/sun/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("linanan1");
         System.out.println("linanan2");
+        System.out.println("master test");
     }
 }
